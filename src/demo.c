93c05912ccac3aba57048c820b031581cc32dae0
Lauri Gustafsson <me@gustafla.space>
2023-08-04 18:53:20 +0300

    Fix noise texture internalformat

--- src/demo.c
+++ src/demo.c
@@ -147,8 +147,8 @@ demo_t *demo_init(int width, int height) {
     glActiveTexture(GL_TEXTURE0);
     glGenTextures(1, &demo->noise_texture);
     glBindTexture(GL_TEXTURE_2D, demo->noise_texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, NOISE_SIZE, NOISE_SIZE, 0,
-                 GL_RED, GL_UNSIGNED_BYTE, NULL);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, NOISE_SIZE, NOISE_SIZE, 0, GL_RGBA,
+                 GL_UNSIGNED_BYTE, NULL);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
